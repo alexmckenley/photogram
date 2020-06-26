32f6e276e47f366202accbcc86a5d3c2ee196d3b
Alex Taylor <alexmckenley@gmail.com>
2020-06-26 01:58:53 -0700

    accelstepper +async web server

--- stepper_server/stepper_server.ino
+++ stepper_server/stepper_server.ino
@@ -5,14 +5,21 @@
 #include <ESP8266WiFi.h>          //ESP8266 Core WiFi Library (you most likely already have this in your sketch)
 
 #include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
-#include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
-#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
 #include <ESP8266mDNS.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <ESPAsyncWiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
+#include <AccelStepper.h>
 
 #define STEP_PIN 5
 #define DIR_PIN 4
 
-ESP8266WebServer server(80);
+AsyncWebServer server(80);
+DNSServer dns;
+AccelStepper stepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN);
+
+unsigned long lastUpdate = 0;
+
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -25,7 +32,7 @@ void setup() {
   digitalWrite(DIR_PIN, LOW);
 
   // Setup WIFIManager
-  WiFiManager wifiManager;
+  AsyncWiFiManager wifiManager(&server, &dns);
   wifiManager.autoConnect("photogram_SETUP", "photogram");
 
   // Setup MDNS
@@ -34,84 +41,103 @@ void setup() {
   }
 
   // Setup web server
-  server.on("/", handleRoot);
-  server.on("/move", handleMove);
+  server.on("/", HTTP_GET, handleRoot);
+  server.on("/move", HTTP_GET, handleMove);
   server.onNotFound(handleNotFound);
   server.begin();
+
+  // Setup stepper
+  stepper.setMaxSpeed(1000);
 
   Serial.println("HTTP server started");
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  server.handleClient();
   MDNS.update();
+  stepper.runSpeed();
 }
 
 void doMove(long deg, long dir) {
-  for (int i = 0; i < 500; i++) {
+  int fullRevolutionSteps = 3200;
+  int steps = (deg / (float)360) * fullRevolutionSteps;
+
+  for (int i = 0; i < steps; i++) {
     digitalWrite(STEP_PIN, HIGH);   // turn the LED on (HIGH is the voltage level)
     delayMicroseconds(1000);
     digitalWrite(STEP_PIN, LOW);    // turn the LED off by making the voltage LOW
     delayMicroseconds(1000);
   }
 }
-
-void handleRoot() {
-  doMove(200, 200);
-
-  server.send(200, "text/plain", "I'm alive!! \n\n" + String(random(1000)));
+void moveOneStep() {
+  digitalWrite(STEP_PIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delayMicroseconds(10);
+  digitalWrite(STEP_PIN, LOW);    // turn the LED off by making the voltage LOW
 }
 
-void handleMove() {
+void handleRoot(AsyncWebServerRequest *request) {
+  request->send(200, "text/plain", "I'm alive!! \n\n" + String(random(1000)));
+}
+
+void handleMove(AsyncWebServerRequest *request) {
   long degParam = 0;
   long dirParam = 0;
-  for (uint8_t i = 0; i < server.args(); i++) {
-    if (server.argName(i) == "deg") {
-      degParam = String(server.arg(i)).toInt();
-      if (degParam == 0) {
-        server.send(400, "text/plain", "invalid deg param, got: \"" + server.arg(i) + "\"");
-        return;
-      }
-    }
-    if (server.argName(i) == "dir") {
-      dirParam = String(server.arg(i)).toInt();
-      if (dirParam == 0) {
-        server.send(400, "text/plain", "invalid dir param, got: \"" + server.arg(i) + "\"");
-        return;
-      }
+  if (request->hasParam("dir")) {
+    AsyncWebParameter* p = request->getParam("dir");
+    dirParam = String(p->value().c_str()).toInt();
+    if (dirParam < 0 || dirParam > 1) {
+      request->send(400, "text/plain", "invalid dir param, got: \"" + String(p->value().c_str()) + "\"[" + dirParam + "]\"");
+      return;
     }
   }
-  if (degParam == 0 || dirParam == 0) {
+  if (request->hasParam("deg")) {
+    AsyncWebParameter* p = request->getParam("deg");
+    degParam = String(p->value().c_str()).toInt();
+    if (degParam == 0 || degParam > 720) {
+      request->send(400, "text/plain", "invalid deg param, got: \"" + String(p->value().c_str()) + "\"[" + degParam + "]");
+      return;
+    }
+  }
+  if (degParam < 1 || degParam > 720 || dirParam < 0 || dirParam > 1) {
     String message = "Invalid input\n\n";
     message += "URI: ";
-    message += server.uri();
+    message += request->url();
     message += "\nMethod: ";
-    message += (server.method() == HTTP_GET) ? "GET" : "POST";
-    message += "\nArguments: ";
-    message += server.args();
-    message += "\n";
-    for (uint8_t i = 0; i < server.args(); i++) {
-      message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+    message += (request->method() == HTTP_GET) ? "GET" : "POST";
+    message += "\nArguments: \n";
+    int params = request->params();
+    for (int i = 0; i < params; i++) {
+      AsyncWebParameter* p = request->getParam(i);
+      message += String(sprintf("GET[%s]: %s\n", p->name().c_str(), p->value().c_str()));
     }
-    server.send(400, "text/plain", message);
+    request->send(400, "text/plain", message);
     return;
   }
-  doMove(degParam, dirParam);
-  server.send(200, "text/plain", "success");
+
+  int steps = (degParam / (float)360) * 3200;
+  stepper.move(steps);
+  
+  String message = "success \ndeg: " + String(degParam) + "\ndir: " + String(dirParam);
+  request->send("text/plain", message.length(), [message](uint8_t *buffer, size_t maxLen, size_t index) -> size_t {
+    if(stepper.isRunning()) {
+      return 0;
+    }
+    memcpy(buffer, message.c_str(), message.length());
+    return message.length();
+  });
 }
 
-void handleNotFound() {
+void handleNotFound(AsyncWebServerRequest *request) {
   String message = "File Not Found\n\n";
   message += "URI: ";
-  message += server.uri();
+  message += request->url();
   message += "\nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i = 0; i < server.args(); i++) {
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  message += (request->method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: \n";
+  int params = request->params();
+  for (int i = 0; i < params; i++) {
+    AsyncWebParameter* p = request->getParam(i);
+    message += String(sprintf("GET[%s]: %s\n", p->name().c_str(), p->value().c_str()));
   }
-  server.send(404, "text/plain", message);
+  request->send(404, "text/plain", message);
 }
